676d1a17dc80c53e6a513893b7bde81149dcc0db
Pawberry <pawberry.works@gmail.com>
2026-06-07 17:46:40 +0200

    Brand logo in both themes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,18 @@
+import LogoDarkTheme from "../assets/logoDarkTheme.png";
+import LogoLightTheme from "../assets/logoLightTheme.png";
+import "./Header.css";
+
 function Header({ theme, onToggleTheme }) {
   const nextTheme = theme === "light" ? "dark" : "light";
 
+   const logoTheme = () => {
+    return theme === "light" ? LogoLightTheme : LogoDarkTheme;
+  };
+
   return (
     <header className="app-header">
+      <img src={logoTheme()} alt="K-pop moodboard logo" className="logoTheme" />
+      {/* <img src={Logo} alt="K-pop moodboard logo" className="logo" /> */}
       <div>
         <p className="eyebrow">your cozy k-pop space</p>
       </div>
