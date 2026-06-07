f3cf437354be8ad4deee9584fbded3873ddb066c
Pawberry <pawberry.works@gmail.com>
2026-06-07 16:46:41 +0200

    Brand color changes + header

---
+++ src/components/Header.jsx
@@ -0,0 +1,24 @@
+function Header() {
+  return (
+    <header className="app-header">
+      { <div>
+        <p className="eyebrow">your cozy k-pop space</p>
+      </div>
+      
+      
+      /* <div>
+        <p className="eyebrow">your cozy k-pop space</p>
+        <h1>K-pop Moodboard</h1>
+        <p className="subtitle">
+          Save, sort and rediscover your favorite comfort content.
+        </p>
+      </div>
+
+      <button className="header-button">
+        Add new item
+      </button> */}
+    </header>
+  );
+}
+
+export default Header;
